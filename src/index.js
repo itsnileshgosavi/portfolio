326965f0ad31b826a02fdc2d0099546c0c40cd63
Nilesh <nbgosavi5@gmail.com>
2024-06-17 20:59:34 +0530

    added corousal for projects added animations

--- src/index.js
+++ src/index.js
@@ -46,31 +46,12 @@ document.querySelectorAll("section").forEach((section) => {
 
 //dark night functionality
 const darkModeToggle = document.getElementById("darkModeToggle");
-  const darkModeToggleMb = document.getElementById("darkModeToggleMb");
-  const htmlTag = document.getElementById("htmlTag");
+const darkModeToggleMb = document.getElementById("darkModeToggleMb");
+const htmlTag = document.getElementById("htmlTag");
+const localStorageTheme = localStorage.theme;
+
 document.addEventListener("DOMContentLoaded", function () {
   
-  
-  darkModeToggle.addEventListener("click", function () {
-    htmlTag.classList.toggle("dark");
-    if(localStorage.theme =="light"){
-      localStorage.theme ="dark"
-
-    }else if(localStorage.theme =="dark"){
-      localStorage.theme ="light"
-    }
-    
-  });
-  darkModeToggleMb.addEventListener("click", function () {
-    htmlTag.classList.toggle("dark");
-    if(localStorage.theme =="light"){
-      localStorage.setItem("theme", "dark")
-
-    }else if(localStorage.theme =="dark"){
-      localStorage.setItem("theme", "light")
-    }
-  });
-
 // making the system preference as default dark/light mode
 if (
   localStorage.theme === "dark" ||
@@ -84,4 +65,109 @@ if (
   document.documentElement.classList.remove("dark");
 }
 });
+
+darkModeToggle.addEventListener("click", function () {
+  htmlTag.classList.toggle("dark");
+  if(localStorageTheme){
+    if(localStorageTheme==="dark"){
+      localStorage.theme="light"
+    }else{
+      localStorage.theme="dark"
+    }
+  }else{
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+      localStorage.theme="light"
+    }else{
+      localStorage.theme="dark"
+    }
+  }
+  
+});
+darkModeToggleMb.addEventListener("click", function () {
+  htmlTag.classList.toggle("dark");
+  if(localStorageTheme){
+    if(localStorageTheme==="dark"){
+      localStorage.theme="light"
+    }else{
+      localStorage.theme="dark"
+    }
+  }else{
+    if(window.matchMedia("(prefers-color-scheme: dark)").matches){
+      localStorage.theme="light"
+    }else{
+      localStorage.theme="dark"
+    }
+  }
+});
+
+const mybutton = document.getElementById("btn-back-to-top");
+
+const scrollFunction = () => {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.classList.remove("hidden");
+  } else {
+    mybutton.classList.add("hidden");
+  }
+  console.log(document.documentElement.scrollTop)
+ 
+    const skillElements = document.querySelectorAll(".progress");
+    const radialProgressEle = document.querySelectorAll('.radial-progress');
+    radialProgressEle.forEach(element=>{
+      const targetValue = element.style.getPropertyValue('--value');
+      element.style.setProperty('--final-value', targetValue);
+      if(document.documentElement.scrollTop>1440 && document.documentElement.scrollTop<2400){
+        element.classList.add("animate-radial-progress");
+        }else{
+          element.classList.remove("animate-radial-progress")
+        }
+    })
+    skillElements.forEach(element=>{
+      const width = element.getAttribute('data-width');
+      element.style.setProperty('--dynamic-width', width);
+      if(document.documentElement.scrollTop>1440 && document.documentElement.scrollTop<2500){
+      element.classList.add("animate-progress");
+      }else{
+        element.classList.remove("animate-progress")
+      }
+    })
+  
+};
+
+scrollFunction();
+
+const backToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+window.addEventListener("scroll", scrollFunction);
+
+//animating projects
+document.addEventListener("DOMContentLoaded", function() {
+  const carousalItems = document.querySelectorAll('.animate-p');
+
+  // Initialize Intersection Observer
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Add the animation class when the element is in the viewport
+        entry.target.classList.add('flip-in-ver-left');
+      } else {
+        // Remove the animation class when the element exits the viewport
+        entry.target.classList.remove('flip-in-ver-left');
+      }
+    });
+  }, { threshold: 0.1 }); // Trigger when at least 10% of the element is visible
+
+  // Observe each carousal item
+  carousalItems.forEach(item => {
+    observer.observe(item);
+  });
+});
+
 
